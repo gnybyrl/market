fd87530366e208bb5e81a565eb56d17a7ae9c0cd
gnybyrl <gnybyrl@gmail.com>
2026-03-27 02:52:24 +0400

    update

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ function getCategory() {
     fetch('https://69b3c331e224ec066bdd03a6.mockapi.io/api/v1/Category')
         .then(res => res.json())
         .then(categ => {
-            categ.map(categ => categoryList.innerHTML += `<li onclick="sortData('${categ.name}')" class="bg-grey-400 p-2 border border-green-400 rounded-xl cursor-pointer text-green-400 text-md">${categ.name}</li>`)
+            categ.map(categ => categoryList.innerHTML += `<li onclick="sortData('${categ.name}')" class=" p-2 border border-green-400 rounded-xl cursor-pointer text-green-400 text-md">${categ.name}</li>`)
         }
     )
 }
@@ -28,6 +28,7 @@ function getDataRender(data) {
         ` <div class="border border-green-400 p-3 h-fit rounded-lg">
             <img src="${item.image}" class="w-full h-full object-cover rounded">
                 <h4 class="mt-2 font-bold">${item.title}</h4>
+                <h5 class="mt-2 text-yellow-400 italic">${item.category}</h5>
                 <p class="text-green-400">${item.price} AZN</p>
         </div>`
     ).join('')

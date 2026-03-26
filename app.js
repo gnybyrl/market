905a5e4434570cd7b9835d64b936483f77232680
gnybyrl <gnybyrl@gmail.com>
2026-03-27 02:38:20 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+let cardList = document.getElementById('cardList')
+let categoryList = document.getElementById('categoryList')
+let data = []
+
+
+function getCategory() {
+    fetch('https://69b3c331e224ec066bdd03a6.mockapi.io/api/v1/Category')
+        .then(res => res.json())
+        .then(categ => {
+            categ.map(categ => categoryList.innerHTML += `<li onclick="sortData('${categ.name}')" class="bg-grey-400 p-2 border border-green-400 rounded-xl cursor-pointer text-green-400 text-md">${categ.name}</li>`)
+        }
+    )
+}
+
+getCategory()
+
+fetch('https://69b3c331e224ec066bdd03a6.mockapi.io/api/v1/Product')
+    .then(res => res.json())
+    .then(getData => {
+        data = getData
+        getDataRender(data)
+    }
+)
+
+
+function getDataRender(data) {
+    cardList.innerHTML = data.map(item =>
+        ` <div class="border border-green-400 p-3 h-fit rounded-lg">
+            <img src="${item.image}" class="w-full h-full object-cover rounded">
+                <h4 class="mt-2 font-bold">${item.title}</h4>
+                <p class="text-green-400">${item.price} AZN</p>
+        </div>`
+    ).join('')
+}
+
+
+function sortData(name) {
+    const result = (name === 'all') ? data : data.filter(filt => filt.category === name)
+    getDataRender(result)
+
+    if (result.length === 0) {
+        cardList.innerHTML = `<p class= "col-span-full text-center text-2xl text-green-400 mt-10 ">Bu kateqoriyada mehsul movcud deyil. Xahis olunur basqa kateqoriyalara goz atasiniz. </p>`
+    } else {
+        getDataRender(result)
+    }
+
+}
+
